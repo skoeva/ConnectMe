bb7ea712a31c5248171046919020ab48ee4e7fa2
Evangelos <eas458@cornell.edu>
2023-03-25 11:14:25 -0400

    Created similarity matrix, simplified queries

--- backend/app.py
+++ backend/app.py
@@ -30,8 +30,37 @@ CORS(app)
 # but if you decide to use SQLAlchemy ORM framework,
 # there's a much better and cleaner way to do this
 
+# Create similarity matrix
+query = f"""SELECT * FROM responses"""
+data = mysql_engine.query_selector(query)
+results = data.fetchall()
 
-def calculate_similarity(name1, name2):
+mat = [[0] * len(results)] * len(results)
+
+for i in range(len(results)):
+    for j in range(len(results)):
+        if results[i][0] == results[j][0]:
+            mat[i][j] == "{:0.2f}".format(1.00)
+
+        sim = sum([((10 - abs(v1 - v2)) / (10*(len(results[0])-1))) for v1,
+                   v2 in zip(results[i][1:], results[j][1:])])
+
+        mat[i][j] = "{:0.2f}".format(sim)
+
+# Create name to index map
+name_to_index_map = {}
+for i, user in enumerate(results):
+    name_to_index_map[user[0]] = i
+
+
+# Calculate similarity between two users
+def calculate_similarity(name1, name2, sim_matrix):
+    print(name1, name2, sim_matrix[name_to_index_map[name1]]
+          [name_to_index_map[name2]])
+    return sim_matrix[name_to_index_map[name1]][name_to_index_map[name2]]
+
+
+# def calculate_similarity(name1, name2):
     # Query for users 'name1' and 'name2'
     query_sql = f"""SELECT * FROM responses WHERE LOWER( name ) LIKE '%%{name1.lower()}%%' OR LOWER( name ) LIKE '%%{name2.lower()}%%'"""
     data = mysql_engine.query_selector(query_sql)
@@ -52,13 +81,42 @@ def calculate_similarity(name1, name2):
     return "{:0.2f}".format(sim)
 
 
-def create_similarity_mat():
-    table = f"""SELECT * FROM responses"""
-    data = mysql_engine.query_selector(table)
+# def crazycrazy(name1, name2):
+    query = f"""SELECT * FROM responses"""
+    data = mysql_engine.query_selector(query)
     results = data.fetchall()
 
     mat = [[0] * len(results)] * len(results)
-    return 0
+
+    for i in range(len(results)):
+        for j in range(len(results)):
+            if results[i][0] == results[j][0]:
+                mat[i][j] == "{:0.2f}".format(1.00)
+
+            sim = sum([((10 - abs(v1 - v2)) / (10*(len(results[0])-1))) for v1,
+                      v2 in zip(results[i][1:], results[j][1:])])
+
+            mat[i][j] = "{:0.2f}".format(sim)
+
+    for i in range(len(results)):
+        for j in range(len(results)):
+            if (results[i][0] == name1 and results[j][0] == name2) or (results[j][0] == name1 and results[i][0] == name2):
+                # print(name1, name2, mat[i][j])
+                return mat[i][j]
+
+
+# def create_similarity_mat(calc_sim_func):
+    query = f"""SELECT * FROM responses"""
+    data = mysql_engine.query_selector(query)
+    results = data.fetchall()
+
+    mat = [[0] * len(results)] * len(results)
+
+    for i in range(len(results[0])):
+        for j in range(len(results[0])):
+            mat[i][j] = calc_sim_func(results[i][0], results[j][0])
+    print(mat)
+    return mat
 
 
 # @app.route("/")
@@ -71,6 +129,8 @@ def create_similarity_mat():
 #     text = request.args.get("name")
 #     return calculate_similarity(text)
 
-calculate_similarity('William', 'James')
+# create_similarity_mat(calculate_similarity)
+# calculate_similarity('William', 'James')
+calculate_similarity('William', 'Joseph', mat)
 
 # app.run(debug=True)
